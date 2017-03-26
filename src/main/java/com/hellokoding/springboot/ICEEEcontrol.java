64a6771729848f4a194115e7a56ca26a1d4c6101
nguyenthanhphuoc <14110158@student.hcmute.edu.vn>
2017-03-26 19:14:39 +0700

    tuan7

---
+++ src/main/java/com/hellokoding/springboot/ICEEEcontrol.java
@@ -0,0 +1,14 @@
+package com.hellokoding.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+@Controller
+public class ICEEEcontrol {
+	@RequestMapping("/WebApp")
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
+        return "WELCOME TO ICSSE 2017";
+    }
+}
